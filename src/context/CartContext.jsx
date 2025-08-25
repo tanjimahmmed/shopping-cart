c1488005103a8f3ff058619d4bd92a7ef1cb4666
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-25 20:21:39 +0600

    add localstorage for save the data

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,16 @@
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
 
 const CartContext = createContext();
 
 export function CartProvider({children}) {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        const stored = localStorage.getItem('cart');
+        return stored ? JSON.parse(stored) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
 
     const addToCart = (product) => {
         setCart((prev) => {
